dc6daf86af4b29ede08495972a1aa4193a941315
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2023-04-01 10:32:03 +0500

    done

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('author',views.author, name='author'),
     path('contact',views.contact, name='contact'),
+    path('category',views.category, name='category'),
+    path('tags',views.tags, name='tags'),
+    path('single-post',views.single_post, name='single_post'),
+    path('search',views.search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,5 +12,21 @@ def contact(request):
   context = {'title': 'Contact'}
   return render(request, 'pages/contact.html', context)
 
+def category(request):
+  context = {'title': 'Category'}
+  return render(request, 'pages/category.html', context)
+
+def tags(request):
+  context = {'title': 'Tags'}
+  return render(request, 'pages/tags.html', context)
+
+def single_post(request):
+  context = {'title': 'Single Post'}
+  return render(request, 'pages/single_post.html', context)
+
+def search(request):
+  context = {'title': 'Search'}
+  return render(request, 'pages/search.html', context)
+
 def handler_404(request, exception):
     return render(request, 'pages/404.html', {})
